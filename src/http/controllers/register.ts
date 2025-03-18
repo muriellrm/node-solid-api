300905f5b3a917ff377f10d66a9e3adeb50b64a1
Muriel Rampinelli Mazuchetti <muriel.rampinelli@gmail.com>
2025-03-18 20:20:38 -0300

    feat: adicionando hash na senha

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from "#/lib/prisma";
+import { hash } from "bcryptjs";
 import type { RouteHandlerMethod } from "fastify";
 import { z } from "zod";
 
@@ -9,12 +10,24 @@ export const register: RouteHandlerMethod = async (request, reply) => {
     password: z.string().min(6),
   });
 
-  const { password, ...rest } = bodySchema.parse(request.body);
+  const { password, email, name } = bodySchema.parse(request.body);
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send();
+  }
 
   await prisma.user.create({
     data: {
-      ...rest,
-      password_hash: password,
+      email, 
+      name,
+      password_hash,
     },
   });
 
