da3b7b6c8bcfa80174aab6166824c244a2c9e6c0
Muriel Rampinelli Mazuchetti <muriel.rampinelli@gmail.com>
2025-03-18 20:05:33 -0300

    feat: adicionando banco de dados e primeira rota

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
+
 import fastify from "fastify";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
 
 export const app = fastify();
+
+app.post("/users", async (request, reply)=>{
+  const bodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+  });
+
+  const {password, ...rest} = bodySchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      ...rest,
+      password_hash: password
+    }
+  })
+
+  return reply.status(201).send();
+});
