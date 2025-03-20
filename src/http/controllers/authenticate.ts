ce5672ecf32c582d10999343ad25cb53527dfefa
Muriel Rampinelli Mazuchetti <muriel.rampinelli@gmail.com>
2025-03-19 21:57:03 -0300

    feat: add register and authenticate factories

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,6 +1,5 @@
-import { PrismaUsersRepository } from "#/repositories/prisma/prisma-users-repository";
-import { AuthenticateUseCase } from "#/use-cases/authenticate";
 import { InvalidCredentialsError } from "#/use-cases/errors/invalid-credentials-error";
+import { makeAuthenticateUseCase } from "#/use-cases/factories/make-authenticate-use-case";
 import type { RouteHandlerMethod } from "fastify";
 import { z } from "zod";
 
@@ -12,9 +11,8 @@ export const authenticate: RouteHandlerMethod = async (request, reply) => {
 
   const { email, password } = bodySchema.parse(request.body);
 
-  try {
-    const usersRepository = new PrismaUsersRepository();
-    const authenticateUseCase = new AuthenticateUseCase(usersRepository);
+  try {    
+    const authenticateUseCase = makeAuthenticateUseCase();
     await authenticateUseCase.execute({ email, password });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,5 @@
-import { PrismaUsersRepository } from "#/repositories/prisma/prisma-users-repository";
 import { UserAlreadyExistsError } from "#/use-cases/errors/user-already-exists-error";
-import { RegisterUseCase } from "#/use-cases/register";
+import { makeRegisterUseCase } from "#/use-cases/factories/make-register-use-case";
 import type { RouteHandlerMethod } from "fastify";
 import { z } from "zod";
 
@@ -14,8 +13,7 @@ export const register: RouteHandlerMethod = async (request, reply) => {
   const { email, name, password } = bodySchema.parse(request.body);
 
   try {
-    const usersRepository = new PrismaUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+    const registerUseCase = makeRegisterUseCase();
     await registerUseCase.execute({ email, name, password });
   } catch (error) {
     if (error instanceof UserAlreadyExistsError) {
